c48be6ccb54cbd04826e253508b6349cc36919d6
Alf Bjørn Hustoft <abhustoft@gail.com>
2021-12-05 14:10:25 +0100

    Initial commit

---
+++ src/juniorReading.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+from mappings import getDNColumns
+from fileUtils import get_files
+from columnNameToIndex import column_to_number
+
+DNcolumnsIndexToName = getDNColumns()
+csvFiles, excelFiles = get_files()
+#print(csvFiles)
+#print(excelFiles)
+
+#junior = pd.read_excel(os.getcwd() + "/src/from-supplier/" + "junior.xlsx", dtype={'EAN/UPC': str}, usecols=columns)
+#junior = pd.read_excel(os.getcwd() + "/src/from-supplier/" + "junior.xlsx", usecols=columns)
+
+print("  ")
+print("Looper csv fil:")
+
+for csvFile in csvFiles:
+    csvDF = pd.read_csv(os.getcwd() + "/src/from-supplier/" + csvFile, delimiter=";")
+
+    for index in DNcolumnsIndexToName:
+        csvDF.rename(columns={csvDF.columns[index]: DNcolumnsIndexToName[index]}, inplace=True)
+
+    # Drop columns not in DNcolumnsIndexToName
+    allColumnNames = set(csvDF.columns)
+    DNColumnNames = set(DNcolumnsIndexToName.values())
+    deleteColumns = allColumnNames - DNColumnNames
+    csvDF.drop(deleteColumns, axis=1, inplace=True)
+
+# for excelFile in excelFiles:
+#     print("Reading ",excelFile)
+#     excelDF = pd.read_excel(os.getcwd() + "/src/from-supplier/" + excelFile)
+
+print('Read csv:')
+print(csvDF.info())
+print(csvDF.head())
+csvDF.to_csv("file_name.csv", encoding='utf-8', index=False)
+# print(excelDF.info())
---
+++ src/mappings.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+from pathlib import Path
+from columnNameToIndex import column_to_number
+import string
+
+def getDNColumns():
+    p = Path(os.getcwd() + "/src/from-supplier/" + "column-map-datanova.json")
+
+    with p.open('r+') as file:
+        fileString = file.read()          # reads the file in as a long string
+        fileString = '[' + fileString + ']'  # adds brackets to the start and end of the string
+
+    columnsDF = pd.read_json(fileString)
+
+    # Column letters to index number
+    for name, values in columnsDF.iteritems():
+        columnsDF[name] = column_to_number(values[0])
+
+    columnsDict = columnsDF.to_dict('records')[0]
+    columnsIndexToName = dict((value, key) for key, value in columnsDict.items())
+    return columnsIndexToName
