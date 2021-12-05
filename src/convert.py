479a5cb772b0aafa7e24209622dc8f8773226f5e
Alf Bjørn Hustoft <abhustoft@gail.com>
2021-12-05 15:36:23 +0100

    File counter

--- src/convert.py
+++ src/convert.py
@@ -1,33 +1,27 @@
-import pandas as pd
 import os
 import sys
+import pandas as pd
+
 from utils.mappings import getDNColumns
 from utils.fileUtils import *
 from utils.columnNameToIndex import column_to_number
 
-season = sys.argv[1]
-supplier = sys.argv[2]
-
-print("Fetching data from: ", season, " ", supplier)
+season, supplier = sys.argv[1:]
 path = os.getcwd() + "/../Datanova-import/Suppliers/" + season + "/" + supplier + "/from-supplier/"
-print("Fetching data from path: ", path)
-
 
 DNcolumnsIndexToName = getDNColumns(path)
 csvFiles, excelFiles = get_files(path)
 print("Found CSVs: ", csvFiles)
 print("Found excels: ", excelFiles)
 
-for csvFile in csvFiles:
+for fileNo,csvFile in enumerate(csvFiles):
+    print("File no: ", fileNo, csvFile)
     csvDF = pd.read_csv(path + csvFile, delimiter=";")
 
     for index in DNcolumnsIndexToName:
         csvDF.rename(columns={csvDF.columns[index]: DNcolumnsIndexToName[index]}, inplace=True)
 
-    # Drop columns not in DNcolumnsIndexToName
-    allColumnNames = set(csvDF.columns)
-    DNColumnNames = set(DNcolumnsIndexToName.values())
-    deleteColumns = allColumnNames - DNColumnNames
+    deleteColumns = set(csvDF.columns) - set(DNcolumnsIndexToName.values())
     csvDF.drop(deleteColumns, axis=1, inplace=True)
 
 # for excelFile in excelFiles:
